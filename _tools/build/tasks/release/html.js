49d732f74a6237ace0b86dfde99c7655fd604f0d
Rian Verhagen <rian@superherocheesecake.com>
2018-05-04 20:08:30 +0200

    multiple main html files

--- _tools/build/tasks/release/html.js
+++ _tools/build/tasks/release/html.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var handlebars = require('gulp-compile-handlebars');
 var fs = require('fs');
+var browserSync = require('browser-sync').get('server');
 
 module.exports = function (gulp, config, version) {
     return function () {
@@ -15,14 +16,14 @@ module.exports = function (gulp, config, version) {
         //Create templatedata object and add version variable
         var baseTemplateData = Object.assign({}, masterContents, {version:version});
 
-        //Render default HTML template
-        renderHTML('', dest, baseTemplateData);
+        var locales = [];
 
         //Look for other locales files, loop through them and export html templates for each
         fs.readdir(config.lang.src, function(err, items) {
             items.forEach(function(item) {
                 if(!item.match(/\.json$/gi) || item.match(/master\.json/gi)) return;
                 var locale = item.replace('.json', '');
+                locales.push(locale);
                 var localeDest = (locale) ? dest + locale : dest;
 
                 var localeContents = JSON.parse(fs.readFileSync(config.lang.src + '/' + item));
@@ -48,12 +49,19 @@ module.exports = function (gulp, config, version) {
             }
 
             gulp.src(html)
-                .pipe(handlebars(Object.assign({}, templateData, {locale:locale}), options))
+                .pipe(handlebars(Object.assign({}, templateData, {locale:locale, locales:locales}), options))
                 .on('error', function(e){
                     console.error('Error rendering template for locale ' + locale + ': ' + e.message);
                 })
                 .pipe(gulp.dest(dest));
+
+
         }
+
+        //Render default HTML template
+        renderHTML(config.lang.default_locale, dest, baseTemplateData);
+
+        browserSync.reload();
 
     };
 
