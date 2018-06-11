0def0f92682b4cb3e3890d961d3699040668df00
Rian Verhagen <rian@superherocheesecake.com>
2018-06-11 16:50:10 +0200

    small fixes

--- _tools/build/tasks/develop/javascript-app.js
+++ _tools/build/tasks/develop/javascript-app.js
@@ -12,7 +12,7 @@ module.exports = function(gulp, config) {
         webpack(webpackConfig, function(error, stats) {
             if (error) throw new Error('webpack error', error);
             var statsErrors = stats.toString('errors-only');
-            if (statsErrors) console.log('[webpack]', statsErrors);
+            if (statsErrors) console.log(logSymbols.error, statsErrors);
             console.log(logSymbols.success, 'Application JavaScript succesfully generated');
             browserSync.reload();
             callback();
