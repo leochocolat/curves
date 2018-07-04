bd436240f581a673143322ed099ecb03ce4d3ef3
P Coolen <peter@superherocheesecake.com>
2018-07-04 15:14:10 +0200

    lang and scss fixes

--- _tools/build/tasks/shared/lang.js
+++ _tools/build/tasks/shared/lang.js
@@ -1,13 +1,14 @@
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var browserSync = require('browser-sync').get('server');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const browserSync = require('browser-sync').get('server');
+const logSymbols = require('log-symbols');
 
 module.exports = function(gulp, config, version) {
 
     return function() {
 
-        var src = config.lang.src;
-        var dest = config.lang.dest;
+        const src = config.lang.src;
+        const dest = config.lang.dest;
 
         // create path if not existing
         mkdirp.sync(dest + '/', function(err) {
@@ -18,20 +19,20 @@ module.exports = function(gulp, config, version) {
         });
 
         //Load in master language file!
-        var masterContents = JSON.parse(fs.readFileSync(src + '/master.json'));
+        let masterContents = JSON.parse(fs.readFileSync(src + '/master.json'));
 
         //Create templatedata object and add version variable
-        var baseTemplateData = Object.assign({}, masterContents, {version: version});
+        let baseTemplateData = Object.assign({}, masterContents, {version: version});
 
         //Look for other locales files, loop through them and export json file for each
         fs.readdir(src, function(err, items) {
             items.forEach(function(item) {
                 if (!item.match(/\.json$/gi) || item.match(/master\.json/gi)) return;
-                var locale = item.replace('.json', '');
-                var localeContents = JSON.parse(fs.readFileSync(src + '/' + item));
+                let locale = item.replace('.json', '');
+                let localeContents = JSON.parse(fs.readFileSync(src + '/' + item));
 
                 //Merge locale contents with master file, so it falls back
-                var templateData = Object.assign({}, baseTemplateData, localeContents);
+                let templateData = Object.assign({}, baseTemplateData, localeContents);
 
                 writeFile(locale, dest, templateData);
             });
