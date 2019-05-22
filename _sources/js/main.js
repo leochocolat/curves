55831e46bee974b3b3b7d74af30cdf235ad2eac1
Mathijs Peerboom <mathijs@superherocheesecake.com>
2019-05-22 17:44:51 +0200

    added CSSPlugin by default

--- _sources/js/main.js
+++ _sources/js/main.js
@@ -4,6 +4,10 @@ import Superhero from '@shcc/superhero-js';
 import ApplicationRouter from 'routers/ApplicationRouter';
 import ApplicationView  from 'views/ApplicationView';
 
+import { CSSPlugin }  from 'gsap/all';
+// NOTE: Without this line, CSSPlugin may get dropped by your bundler...
+const plugins = [CSSPlugin];
+
 const namespace = Superhero.createNameSpace('com.projectname');
 namespace.applicationRouter = new ApplicationRouter();
 namespace.applicationView = new ApplicationView({ el: document.getElementById('application') });
